d7d703730d36d43a63f15c087fe3233a9e7ebf0d
Razzi Abuissa <razzi53@gmail.com>
2016-07-03 00:34:49 -0700

    Port over some basic config and abbrevs

---
+++ abbrev_defs.el
@@ -0,0 +1,564 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'Buffer-menu-mode-abbrev-table '())
+
+(define-abbrev-table 'Custom-mode-abbrev-table '())
+
+(define-abbrev-table 'Info-edit-mode-abbrev-table '())
+
+(define-abbrev-table 'Man-mode-abbrev-table '())
+
+(define-abbrev-table 'ag-mode-abbrev-table '())
+
+(define-abbrev-table 'awk-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'bibtex-mode-abbrev-table '())
+
+(define-abbrev-table 'bookmark-bmenu-mode-abbrev-table '())
+
+(define-abbrev-table 'bookmark-edit-annotation-mode-abbrev-table '())
+
+(define-abbrev-table 'bs-mode-abbrev-table '())
+
+(define-abbrev-table 'c++-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'c-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'calendar-mode-abbrev-table '())
+
+(define-abbrev-table 'change-log-mode-abbrev-table '())
+
+(define-abbrev-table 'circe-channel-mode-abbrev-table '())
+
+(define-abbrev-table 'circe-chat-mode-abbrev-table '())
+
+(define-abbrev-table 'circe-mode-abbrev-table '())
+
+(define-abbrev-table 'circe-query-mode-abbrev-table '())
+
+(define-abbrev-table 'circe-server-mode-abbrev-table '())
+
+(define-abbrev-table 'comint-mode-abbrev-table '())
+
+(define-abbrev-table 'command-history-mode-abbrev-table '())
+
+(define-abbrev-table 'completion-list-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-colon-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-javaprop-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-ppd-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-space-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-unix-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-windows-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-xdefaults-mode-abbrev-table '())
+
+(define-abbrev-table 'debugger-mode-abbrev-table '())
+
+(define-abbrev-table 'diff-mode-abbrev-table '())
+
+(define-abbrev-table 'dig-mode-abbrev-table '())
+
+(define-abbrev-table 'display-time-world-mode-abbrev-table '())
+
+(define-abbrev-table 'edebug-eval-mode-abbrev-table '())
+
+(define-abbrev-table 'edit-abbrevs-mode-abbrev-table '())
+
+(define-abbrev-table 'emacs-lisp-byte-code-mode-abbrev-table '())
+
+(define-abbrev-table 'emacs-lisp-mode-abbrev-table
+  '(
+    ("kdb" "kbd" nil 2)
+   ))
+
+(define-abbrev-table 'epa-info-mode-abbrev-table '())
+
+(define-abbrev-table 'epa-key-list-mode-abbrev-table '())
+
+(define-abbrev-table 'epa-key-mode-abbrev-table '())
+
+(define-abbrev-table 'ert-results-mode-abbrev-table '())
+
+(define-abbrev-table 'ert-simple-view-mode-abbrev-table '())
+
+(define-abbrev-table 'eshell-mode-abbrev-table
+  '(
+    ("bci" "brew cask install" nil 1)
+    ("brw" "brew" nil 1)
+    ("l" "ls" nil 2)
+    ("ni" "npm install" nil 1)
+    ("o" "open" nil 1)
+    ("p" "python" nil 2)
+    ("pi" "pip install" nil 2)
+    ("t" "py.test" nil 1)
+    ("w" "cd ~/code/clint" nil 1)
+   ))
+
+(define-abbrev-table 'evil-command-window-mode-abbrev-table '())
+
+(define-abbrev-table 'evil-list-view-mode-abbrev-table '())
+
+(define-abbrev-table 'finder-mode-abbrev-table '())
+
+(define-abbrev-table 'fish-mode-abbrev-table '())
+
+(define-abbrev-table 'flycheck-error-list-mode-abbrev-table '())
+
+(define-abbrev-table 'fundamental-mode-abbrev-table '())
+
+(define-abbrev-table 'gdb-script-mode-abbrev-table '())
+
+(define-abbrev-table 'gfm-mode-abbrev-table '())
+
+(define-abbrev-table 'git-rebase-mode-abbrev-table '())
+
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("abbev" "abbrev" nil 1)
+    ("accoutn" "account" nil 1)
+    ("acn" "can" nil 1)
+    ("addres" "address" nil 1)
+    ("afte" "after" nil 1)
+    ("ahve" "have" nil 1)
+    ("alays" "always" nil 1)
+    ("allowinf" "allowing" nil 1)
+    ("ar" "at" nil 1)
+    ("asyc" "async" nil 1)
+    ("attachign" "attaching" nil 1)
+    ("authorizatin" "Authorization" nil 1)
+    ("autocopmlete" "autocomplete" nil 1)
+    ("automaic" "automatic" nil 1)
+    ("avalible" "available" nil 1)
+    ("beefeor" "before" nil 1)
+    ("befre" "before" nil 1)
+    ("beginging" "beginning" nil 1)
+    ("beofre" "before" nil 1)
+    ("brcked" "bracket" nil 1)
+    ("bufer" "buffer" nil 2)
+    ("buld" "build" nil 1)
+    ("cas" "case" nil 1)
+    ("causees" "causes" nil 1)
+    ("chaon" "chain" nil 1)
+    ("chedked" "checked" nil 1)
+    ("chracters" "characters" nil 1)
+    ("cjhar" "char" nil 1)
+    ("claer" "clear" nil 1)
+    ("cldar" "clear" nil 1)
+    ("closign" "closing" nil 1)
+    ("cmpletino" "completion" nil 1)
+    ("collectd" "collected" nil 1)
+    ("colun" "column" nil 1)
+    ("comand" "command" nil 1)
+    ("comapny" "company" nil 1)
+    ("contat" "contact" nil 1)
+    ("coomit" "commit" nil 1)
+    ("coud" "could" nil 1)
+    ("craeted" "created" nil 1)
+    ("createsa" "creates a" nil 1)
+    ("creidt" "credit" nil 1)
+    ("cty" "City" nil 1)
+    ("curosr" "cursor" nil 1)
+    ("currentl" "currently" nil 1)
+    ("dcionary" "dictionary" nil 1)
+    ("delet" "delete" nil 1)
+    ("denormailzed" "denormalized" nil 1)
+    ("depencencies" "dependencies" nil 1)
+    ("dependences" "dependencies" nil 1)
+    ("descrition" "description" nil 1)
+    ("deserealize" "deserialize" nil 1)
+    ("dfe" "def" nil 1)
+    ("dicitonaries" "dictionaries" nil 2)
+    ("disalbe" "disable" nil 0)
+    ("dlete" "delete" nil 1)
+    ("docsting" "docstring" nil 1)
+    ("eco" "echo" nil 1)
+    ("ecoh" "echo" nil 1)
+    ("eixt" "exit" nil 1)
+    ("emtpy" "empty" nil 1)
+    ("eqaul" "equal" nil 1)
+    ("erognomic" "ergonomic" nil 1)
+    ("errro" "error" nil 1)
+    ("errrors" "errors" nil 1)
+    ("exapmle" "example" nil 1)
+    ("expicity" "explicitly" nil 1)
+    ("feild" "field" nil 1)
+    ("feld" "field" nil 1)
+    ("fiedls" "fields" nil 1)
+    ("flychck" "flycheck" nil 1)
+    ("francisco" "Francisco" nil 1)
+    ("fro" "for" nil 1)
+    ("functino" "function" nil 1)
+    ("glosary" "glossary" nil 1)
+    ("gogole" "google" nil 1)
+    ("gonfirm" "confirm" nil 1)
+    ("greate" "create" nil 1)
+    ("happenign" "happening" nil 1)
+    ("haveing" "having" nil 1)
+    ("hel" "helm" nil 1)
+    ("hiippie" "hippie" nil 1)
+    ("identifer" "identifier" nil 1)
+    ("identiiers" "identifiers" nil 1)
+    ("idnenting" "indenting" nil 1)
+    ("ikn" "in" nil 1)
+    ("imoprt" "import" nil 3)
+    ("imorpt" "import" nil 1)
+    ("impoosble" "impossible" nil 1)
+    ("impor" "import" nil 1)
+    ("imporot" "import" nil 1)
+    ("imporr" "import" nil 1)
+    ("impot" "import" nil 1)
+    ("improt" "import" nil 1)
+    ("imprt" "import" nil 1)
+    ("imprtant" "important" nil 1)
+    ("ina" "in a" nil 1)
+    ("indiviuals" "individuals" nil 1)
+    ("inser" "insert" nil 2)
+    ("inserrt" "insert" nil 1)
+    ("inset" "insert" nil 1)
+    ("instantaited" "instantiated" nil 1)
+    ("intermeidate" "intermediate" nil 1)
+    ("interpeter" "interpreter" nil 1)
+    ("iself" "itself" nil 1)
+    ("isnt" "isn't" nil 1)
+    ("isthis" "is this" nil 1)
+    ("itsef" "itself" nil 1)
+    ("iwth" "with" nil 1)
+    ("jenkisn" "jenkins" nil 1)
+    ("jion" "join" nil 1)
+    ("joiin" "join" nil 1)
+    ("kpb" "kbd" nil 1)
+    ("las" "last" nil 1)
+    ("lbamda" "lambda" nil 1)
+    ("lcl" "LLC" nil 3)
+    ("lista" "list" nil 1)
+    ("listp" "lisp" nil 0)
+    ("lits" "list" nil 1)
+    ("lmit" "limit" nil 1)
+    ("loadn" "loan" nil 1)
+    ("localhst" "localhost" nil 1)
+    ("locla" "local" nil 0)
+    ("maangement" "management" nil 1)
+    ("mak" "make" nil 1)
+    ("makethis" "make this" nil 1)
+    ("mathing" "matching" nil 1)
+    ("mayb" "maybe" nil 1)
+    ("mdir" "mkdir" nil 1)
+    ("memebers" "members" nil 1)
+    ("merics" "metrics" nil 1)
+    ("mertric" "Metric" nil 1)
+    ("mesage" "message" nil 1)
+    ("mkaing" "making" nil 0)
+    ("mst" "must" nil 1)
+    ("mulit" "Multi" nil 1)
+    ("multipel" "multiple" nil 1)
+    ("needef" "needed" nil 1)
+    ("ngineer" "engineer" nil 1)
+    ("nme" "name" nil 1)
+    ("notimpementederror" "NotImplementedError" nil 1)
+    ("nullabke" "nullable" nil 1)
+    ("nwo" "now" nil 0)
+    ("ocmposite" "composite" nil 1)
+    ("oculd" "could" nil 1)
+    ("odesn" "doesn" nil 1)
+    ("ofr" "for" nil 1)
+    ("onwars" "onwards" nil 1)
+    ("oopenin" "opening" nil 1)
+    ("otehr" "other" nil 1)
+    ("otehrwise" "otherwise" nil 1)
+    ("otherise" "otherwise" nil 1)
+    ("outpt" "output" nil 1)
+    ("ovoerwrite" "overwrite" nil 1)
+    ("owdrs" "words" nil 1)
+    ("pacage" "package" nil 2)
+    ("paginatino" "pagination" nil 1)
+    ("pagnation" "pagination" nil 1)
+    ("paht" "path" nil 1)
+    ("paramater" "parameter" nil 1)
+    ("parcen" "parcel" nil 1)
+    ("parentehsis" "parenthesis" nil 1)
+    ("pares" "parse" nil 1)
+    ("paswords" "passwords" nil 1)
+    ("perhas" "perhaps" nil 1)
+    ("pge" "page" nil 1)
+    ("pipelien" "pipeline" nil 1)
+    ("pipelne" "pipeline" nil 1)
+    ("platofrm" "platform" nil 1)
+    ("poewr" "Power" nil 1)
+    ("procesed" "processed" nil 1)
+    ("puthon" "python" nil 1)
+    ("pyhton" "python" nil 2)
+    ("pytho" "python" nil 1)
+    ("pyton" "python" nil 1)
+    ("querionable" "questionable" nil 1)
+    ("qulification" "qualification" nil 1)
+    ("quoes" "quotes" nil 1)
+    ("qutes" "quotes" nil 1)
+    ("quti" "quit" nil 1)
+    ("qutoe" "quote" nil 1)
+    ("rateh" "rate" nil 1)
+    ("razi" "razzi" nil 1)
+    ("reeturn" "return" nil 1)
+    ("relatoinships" "relationships" nil 1)
+    ("relevat" "relevant" nil 1)
+    ("represenattive" "representative" nil 1)
+    ("requeres" "requires" nil 1)
+    ("requier" "require" nil 1)
+    ("requierd" "Required" nil 1)
+    ("restoer" "restore" nil 1)
+    ("restrat" "restart" nil 1)
+    ("reveiew" "review" nil 1)
+    ("riase" "raise" nil 1)
+    ("rwap" "wrap" nil 1)
+    ("saev" "save" nil 1)
+    ("saluation" "salutation" nil 1)
+    ("saluattion" "salutation" nil 1)
+    ("sav" "save" nil 1)
+    ("scheudel" "schedule" nil 1)
+    ("scheudle" "schedule" nil 0)
+    ("scheudles" "schedules" nil 0)
+    ("seach" "search" nil 1)
+    ("searchign" "searching" nil 1)
+    ("serach" "search" nil 3)
+    ("sieres" "series" nil 1)
+    ("sighned" "signed" nil 1)
+    ("slcied" "sliced" nil 1)
+    ("slef" "self" nil 1)
+    ("soem" "some" nil 1)
+    ("solarste" "solarsite" nil 1)
+    ("solarstei" "solarsite" nil 1)
+    ("sring" "string" nil 2)
+    ("sslef" "self" nil 1)
+    ("ssytem" "system" nil 1)
+    ("staet" "state" nil 1)
+    ("steramilned" "streamlined" nil 1)
+    ("sth" "something" nil 1)
+    ("streing" "string" nil 1)
+    ("strng" "string" nil 1)
+    ("styel" "style" nil 1)
+    ("subet" "subset" nil 1)
+    ("sublass" "subclass" nil 1)
+    ("suceed" "succeed" nil 1)
+    ("superclas" "superclass" nil 1)
+    ("surce" "source" nil 1)
+    ("symboc" "symbol" nil 1)
+    ("symobl" "symbol" nil 2)
+    ("sysmte" "system" nil 1)
+    ("systesm" "systems" nil 1)
+    ("sytem" "system" nil 1)
+    ("sytle" "style" nil 1)
+    ("teh" "the" nil 4)
+    ("tgiven" "given" nil 1)
+    ("thesse" "these" nil 1)
+    ("thorugh" "through" nil 1)
+    ("toay" "today" nil 1)
+    ("tocmmoent" "comment" nil 1)
+    ("todo" "TODO" nil 27)
+    ("tood" "TODO" nil 1)
+    ("toodo" "TODO" nil 1)
+    ("toogle" "toggle" nil 1)
+    ("tp" "to" nil 1)
+    ("tye" "the" nil 1)
+    ("udpate" "update" nil 1)
+    ("utilit" "utility" nil 1)
+    ("utilitiies" "utilities" nil 1)
+    ("vales" "values" nil 1)
+    ("vauels" "values" nil 1)
+    ("ver" "very" nil 1)
+    ("vidual" "Visual" nil 1)
+    ("whcih" "which" nil 2)
+    ("whitepace" "whitespace" nil 1)
+    ("wiht" "with" nil 1)
+    ("withoout" "without" nil 2)
+    ("wor" "work" nil 1)
+    ("woudl" "would" nil 1)
+    ("wthout" "without" nil 1)
+    ("wxiting" "exiting" nil 1)
+    ("xpanded" "expanded" nil 1)
+    ("yeild" "yield" nil 1)
+    ("youy" "you" nil 1)
+   ))
+
+(define-abbrev-table 'gnus-article-edit-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-article-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-browse-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-category-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-group-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-score-mode-abbrev-table '())
+
+(define-abbrev-table 'gnus-sticky-article-mode-abbrev-table '())
+
+(define-abbrev-table 'grep-mode-abbrev-table '())
+
+(define-abbrev-table 'gud-mode-abbrev-table '())
+
+(define-abbrev-table 'haskell-cabal-mode-abbrev-table '())
+
+(define-abbrev-table 'haskell-mode-abbrev-table '())
+
+(define-abbrev-table 'helm-ag-mode-abbrev-table '())
+
+(define-abbrev-table 'helm-git-grep-mode-abbrev-table '())
+
+(define-abbrev-table 'helm-grep-mode-abbrev-table '())
+
+(define-abbrev-table 'helm-major-mode-abbrev-table '())
+
+(define-abbrev-table 'helm-moccur-mode-abbrev-table '())
+
+(define-abbrev-table 'help-mode-abbrev-table '())
+
+(define-abbrev-table 'html-mode-abbrev-table '())
+
+(define-abbrev-table 'ibuffer-mode-abbrev-table '())
+
+(define-abbrev-table 'idl-mode-abbrev-table '())
+
+(define-abbrev-table 'image-dired-display-image-mode-abbrev-table '())
+
+(define-abbrev-table 'image-dired-thumbnail-mode-abbrev-table '())
+
+(define-abbrev-table 'inferior-python-mode-abbrev-table '())
+
+(define-abbrev-table 'java-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'js-mode-abbrev-table '())
+
+(define-abbrev-table 'js2-jsx-mode-abbrev-table '())
+
+(define-abbrev-table 'js2-mode-abbrev-table '())
+
+(define-abbrev-table 'json-mode-abbrev-table '())
+
+(define-abbrev-table 'lisp-mode-abbrev-table '())
+
+(define-abbrev-table 'literate-haskell-mode-abbrev-table '())
+
+(define-abbrev-table 'log-edit-mode-abbrev-table '())
+
+(define-abbrev-table 'lui-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-cherry-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-diff-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-log-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-log-select-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-merge-preview-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-popup-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-process-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-reflog-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-refs-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-revision-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-stash-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-stashes-mode-abbrev-table '())
+
+(define-abbrev-table 'magit-status-mode-abbrev-table '())
+
+(define-abbrev-table 'markdown-mode-abbrev-table '())
+
+(define-abbrev-table 'message-mode-abbrev-table '())
+
+(define-abbrev-table 'messages-buffer-mode-abbrev-table '())
+
+(define-abbrev-table 'nxml-mode-abbrev-table '())
+
+(define-abbrev-table 'objc-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'occur-edit-mode-abbrev-table '())
+
+(define-abbrev-table 'occur-mode-abbrev-table '())
+
+(define-abbrev-table 'org-mode-abbrev-table '())
+
+(define-abbrev-table 'outline-mode-abbrev-table '())
+
+(define-abbrev-table 'package-menu-mode-abbrev-table '())
+
+(define-abbrev-table 'pike-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'process-menu-mode-abbrev-table '())
+
+(define-abbrev-table 'prog-mode-abbrev-table '())
+
+(define-abbrev-table 'pt-search-mode-abbrev-table '())
+
+(define-abbrev-table 'py-auto-completion-mode-abbrev-table '())
+
+(define-abbrev-table 'py-ipython-shell-mode-abbrev-table '())
+
+(define-abbrev-table 'py-python-shell-mode-abbrev-table '())
+
+(define-abbrev-table 'python-mode-abbrev-table '())
+
+(define-abbrev-table 'python-mode-skeleton-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'restclient-mode-abbrev-table '())
+
+(define-abbrev-table 'select-tags-table-mode-abbrev-table '())
+
+(define-abbrev-table 'sgml-mode-abbrev-table '())
+
+(define-abbrev-table 'sh-mode-abbrev-table '())
+
+(define-abbrev-table 'shell-mode-abbrev-table '())
+
+(define-abbrev-table 'smime-mode-abbrev-table '())
+
+(define-abbrev-table 'snippet-mode-abbrev-table '())
+
+(define-abbrev-table 'special-mode-abbrev-table '())
+
+(define-abbrev-table 'speedbar-mode-abbrev-table '())
+
+(define-abbrev-table 'tabulated-list-mode-abbrev-table '())
+
+(define-abbrev-table 'tar-mode-abbrev-table '())
+
+(define-abbrev-table 'term-mode-abbrev-table '())
+
+(define-abbrev-table 'text-mode-abbrev-table '())
+
+(define-abbrev-table 'url-cookie-mode-abbrev-table '())
+
+(define-abbrev-table 'vc-git-log-edit-mode-abbrev-table '())
+
+(define-abbrev-table 'vc-git-log-view-mode-abbrev-table '())
+
+(define-abbrev-table 'vc-svn-log-view-mode-abbrev-table '())
+
+(define-abbrev-table 'ycmd-view-mode-abbrev-table '())
---
+++ init.el
@@ -0,0 +1,241 @@
+(defun dotspacemacs/layers ()
+  (setq-default
+   ;; Base distribution to use. This is a layer contained in the directory
+   ;; `+distribution'. For now available distributions are `spacemacs-base'
+   ;; or `spacemacs'. (default 'spacemacs)
+   dotspacemacs-distribution 'spacemacs
+   ;; List of additional paths where to look for configuration layers.
+   ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
+   dotspacemacs-configuration-layer-path '()
+   ;; List of configuration layers to load. If it is the symbol `all' instead
+   ;; of a list then all discovered layers will be installed.
+   dotspacemacs-configuration-layers
+   '(
+     ;; ----------------------------------------------------------------
+     ;; Example of useful layers you may want to use right away.
+     ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
+     ;; <M-m f e R> (Emacs style) to install them.
+     ;; ----------------------------------------------------------------
+     auto-completion
+     ;; better-defaults
+     emacs-lisp
+     git
+     ;; markdown
+     ;; org
+     ;; (shell :variables
+     ;;        shell-default-height 30
+     ;;        shell-default-position 'bottom)
+     ;; spell-checking
+     ;; syntax-checking
+     ;; version-control
+     )
+   ;; List of additional packages that will be installed without being
+   ;; wrapped in a layer. If you need some configuration for these
+   ;; packages, then consider creating a layer. You can also put the
+   ;; configuration in `dotspacemacs/user-config'.
+   dotspacemacs-additional-packages '()
+   ;; A list of packages and/or extensions that will not be install and loaded.
+   dotspacemacs-excluded-packages '()
+   ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
+   ;; are declared in a layer which is not a member of
+   ;; the list `dotspacemacs-configuration-layers'. (default t)
+   dotspacemacs-delete-orphan-packages t))
+
+(defun dotspacemacs/init ()
+  (setq-default
+   ;; If non nil output loading progress in `*Messages*' buffer. (default nil)
+   dotspacemacs-verbose-loading nil
+   ;; Specify the startup banner. Default value is `official', it displays
+   ;; the official spacemacs logo. An integer value is the index of text
+   ;; banner, `random' chooses a random text banner in `core/banners'
+   ;; directory. A string value must be a path to an image format supported
+   ;; by your Emacs build.
+   ;; If the value is nil then no banner is displayed. (default 'official)
+   dotspacemacs-startup-banner nil
+   ;; List of items to show in the startup buffer. If nil it is disabled.
+   ;; Possible values are: `recents' `bookmarks' `projects'.
+   ;; (default '(recents projects))
+   dotspacemacs-startup-lists '(recents projects)
+   ;; Number of recent files to show in the startup buffer. Ignored if
+   ;; `dotspacemacs-startup-lists' doesn't include `recents'. (default 5)
+   dotspacemacs-startup-recent-list-size 10
+   ;; Default major mode of the scratch buffer (default `text-mode')
+   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-themes '(
+                         spacemacs-light
+                         leuven
+                         solarized-light
+                         solarized-dark
+                         spacemacs-dark
+                         monokai
+                         zenburn)
+   dotspacemacs-default-font '("Input"
+                               :size 18
+                               :weight normal
+                               :width normal
+                               :powerline-scale 1.1)
+   ;; The leader key
+   dotspacemacs-leader-key "SPC"
+   ;; The leader key accessible in `emacs state' and `insert state'
+   ;; (default "M-m")
+   dotspacemacs-emacs-leader-key "M-m"
+   ;; Major mode leader key is a shortcut key which is the equivalent of
+   ;; pressing `<leader> m`. Set it to `nil` to disable it. (default ",")
+   dotspacemacs-major-mode-leader-key ","
+   ;; Major mode leader key accessible in `emacs state' and `insert state'.
+   ;; (default "C-M-m)
+   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   ;; These variables control whether separate commands are bound in the GUI to
+   ;; the key pairs C-i, TAB and C-m, RET.
+   ;; Setting it to a non-nil value, allows for separate commands under <C-i>
+   ;; and TAB or <C-m> and RET.
+   ;; In the terminal, these pairs are generally indistinguishable, so this only
+   ;; works in the GUI. (default nil)
+   dotspacemacs-distinguish-gui-tab nil
+   ;; (Not implemented) dotspacemacs-distinguish-gui-ret nil
+   ;; The command key used for Evil commands (ex-commands) and
+   ;; Emacs commands (M-x).
+   ;; By default the command key is `:' so ex-commands are executed like in Vim
+   ;; with `:' and Emacs commands are executed with `<leader> :'.
+   dotspacemacs-command-key ":"
+   ;; Name of the default layout (default "Default")
+   dotspacemacs-default-layout-name "Default"
+   ;; If non nil the default layout name is displayed in the mode-line.
+   ;; (default nil)
+   dotspacemacs-display-default-layout nil
+   ;; If non nil then the last auto saved layouts are resume automatically upon
+   ;; start. (default nil)
+   dotspacemacs-auto-resume-layouts nil
+   ;; Location where to auto-save files. Possible values are `original' to
+   ;; auto-save the file in-place, `cache' to auto-save the file to another
+   ;; file stored in the cache directory and `nil' to disable auto-saving.
+   ;; (default 'cache)
+   dotspacemacs-auto-save-file-location 'cache
+   ;; If non nil then `ido' replaces `helm' for some commands. For now only
+   ;; `find-files' (SPC f f), `find-spacemacs-file' (SPC f e s), and
+   ;; `find-contrib-file' (SPC f e c) are replaced. (default nil)
+   dotspacemacs-use-ido nil
+   ;; If non nil, `helm' will try to minimize the space it uses. (default nil)
+   dotspacemacs-helm-resize nil
+   ;; if non nil, the helm header is hidden when there is only one source.
+   ;; (default nil)
+   dotspacemacs-helm-no-header t
+   ;; If non nil the paste micro-state is enabled. When enabled pressing `p`
+   ;; several times cycle between the kill ring content. (default nil)
+   dotspacemacs-enable-paste-micro-state t
+   ;; If non nil a progress bar is displayed when spacemacs is loading. This
+   ;; may increase the boot time on some systems and emacs builds, set it to
+   ;; nil to boost the loading time. (default t)
+   dotspacemacs-loading-progress-bar t
+   ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
+   ;; (Emacs 24.4+ only)
+   dotspacemacs-fullscreen-at-startup nil
+   ;; If non nil `spacemacs/toggle-fullscreen' will not use native fullscreen.
+   ;; Use to disable fullscreen animations in OSX. (default nil)
+   dotspacemacs-fullscreen-use-non-native nil
+   ;; If non nil the frame is maximized when Emacs starts up.
+   ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
+   ;; (default nil) (Emacs 24.4+ only)
+   dotspacemacs-maximized-at-startup t
+   ;; A value from the range (0..100), in increasing opacity, which describes
+   ;; the transparency level of a frame when it's active or selected.
+   ;; Transparency can be toggled through `toggle-transparency'. (default 90)
+   dotspacemacs-active-transparency 90
+   ;; A value from the range (0..100), in increasing opacity, which describes
+   ;; the transparency level of a frame when it's inactive or deselected.
+   ;; Transparency can be toggled through `toggle-transparency'. (default 90)
+   dotspacemacs-inactive-transparency 90
+   ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
+   ;; scrolling overrides the default behavior of Emacs which recenters the
+   ;; point when it reaches the top or bottom of the screen. (default t)
+   dotspacemacs-smooth-scrolling t
+   ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
+   ;; derivatives. If set to `relative', also turns on relative line numbers.
+   ;; (default nil)
+   dotspacemacs-line-numbers t
+   ;; If non-nil smartparens-strict-mode will be enabled in programming modes.
+   ;; (default nil)
+   dotspacemacs-smartparens-strict-mode nil
+   ;; List of search tool executable names. Spacemacs uses the first installed
+   ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
+   ;; (default '("ag" "pt" "ack" "grep"))
+   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   ;; The default package repository used if no explicit repository has been
+   ;; specified with an installed package.
+   ;; Not used for now. (default nil)
+   dotspacemacs-default-package-repository nil
+   ;; Delete whitespace while saving buffer. Possible values are `all'
+   ;; to aggressively delete empty line and long sequences of whitespace,
+   ;; `trailing' to delete only the whitespace at end of lines, `changed'to
+   ;; delete only whitespace for changed lines or `nil' to disable cleanup.
+   ;; (default nil)
+   dotspacemacs-whitespace-cleanup 'all
+   ))
+
+(defun dotspacemacs/user-init ()
+  "This function is mostly useful for variables that need to be set
+before packages are loaded."
+  (setq-default
+   scroll-margin 0)
+  )
+
+(defun razzi/insert-newline-after()
+  (interactive)
+  (evil-insert-newline-below)
+  (forward-line -1))
+
+(defun razzi/insert-newline-before()
+  (interactive)
+  (evil-insert-newline-above)
+  (forward-line))
+
+(defun razzi/select-symbol ()
+  (interactive)
+  (evil-visual-char)
+  (evil-inner-symbol))
+
+(defun save-if-buffer-is-file ()
+  (if (buffer-file-name)
+    (save-buffer)))
+
+(defun dotspacemacs/user-config ()
+  (evil-leader/set-key
+    "," 'helm-exit-minibuffer
+    "DEL" 'spacemacs/emacs-reload
+    "v" 'razzi/select-symbol)
+
+  (setq
+    evil-escape-key-sequence "jf"
+    evil-escape-unordered-key-sequence t
+    evil-escape-delay 0.4)
+
+  (setq gc-cons-threshold 100000000)
+
+  (setq
+    abbrev-file-name "~/.spacemacs.d/abbrev_defs.el"
+    confirm-nonexistent-file-or-buffer nil)
+
+  (setq-default
+    scroll-margin 0)
+
+  (global-set-key (kbd "C-`") 'describe-key)
+
+  (define-key evil-normal-state-map (kbd "[ SPC") 'razzi/insert-newline-before)
+  (define-key evil-normal-state-map (kbd "] SPC") 'razzi/insert-newline-after)
+  (define-key evil-operator-state-map (kbd "SPC") 'evil-inner-symbol)
+
+  (define-key minibuffer-local-map (kbd "C-j") 'exit-minibuffer)
+  (define-key isearch-mode-map (kbd "C-j") 'isearch-done)
+  (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
+
+  (use-package helm
+    :config
+    (define-key helm-map (kbd "C-j") nil)
+    (define-key helm-map (kbd "C-j") 'helm-maybe-exit-minibuffer)
+    (setq helm-ff-newfile-prompt-p nil)
+    )
+
+  (add-hook 'evil-insert-state-exit-hook 'save-if-buffer-is-file)
+  (defadvice switch-to-buffer (before save-buffer-now activate)
+    (when buffer-file-name (save-buffer)))
+  )
