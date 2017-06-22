e2541951e0bc4efa1961f8e6b8a39379db565c21
Razzi Abuissa <razzi53@gmail.com>
2017-06-22 01:49:01 -0700

    Add neotree toggle to spc 0

--- layers/razzineotree/packages.el
+++ layers/razzineotree/packages.el
@@ -3,7 +3,13 @@
 (defun razzi/neotree-preview ()
   (interactive)
   (spacemacs/neotree-expand-or-open)
-  (select-window-0))
+  (winum-select-window-0-or-10))
+
+(defun razzi/activate-neotree ()
+  (interactive)
+  (if (winum-get-window-by-number 0)
+      (winum-select-window-0)
+    (neotree-toggle)))
 
 (defun razzineotree/post-init-neotree ()
   (use-package neotree
