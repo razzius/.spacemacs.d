e184a0c2c6e0601a9bc3f0f8fd1221af82ef6511
Razzi Abuissa <razzi53@gmail.com>
2019-10-18 14:43:05 -0700

    Add razzi-work package

--- init.el
+++ init.el
@@ -59,6 +59,7 @@ This function should only modify configuration layer settings."
      razzi-tab-completion
      razzi-vc
      razzi-tabs
+     razzi-work
      (version-control :variables
                       git-magit-status-fullscreen t))
 
