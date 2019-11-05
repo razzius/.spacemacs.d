40dd0bf8943b88b97d7b2b48192c75b753545fe7
Razzi Abuissa <razzi53@gmail.com>
2019-11-05 11:13:07 -0800

    helm and vterm

--- layers/razzi-helm/packages.el
+++ layers/razzi-helm/packages.el
@@ -5,10 +5,20 @@
   (let ((default-directory dir))
     (call-interactively 'spacemacs/helm-project-smart-do-search)))
 
+(defun razzi-helm-run-magit-status (dir)
+  (interactive)
+  (let ((default-directory dir))
+    (call-interactively 'magit-status)))
+
 (defun raz-helm-projectile-rg ()
   "Projectile version of `helm-rg'."
   (interactive)
   (helm-exit-and-execute-action 'razzi-helm-run-project-search))
+
+(defun raz-helm-projectile-magit-status ()
+  "Projectile version of `magit-status'."
+  (interactive)
+  (helm-exit-and-execute-action 'razzi-helm-run-magit-status))
 
 (defun razzi-helm/post-init-helm ()
   (use-package helm
@@ -24,4 +34,6 @@
     (global-set-key (kbd "C-/") nil)
     (define-key undo-tree-map (kbd "C-/") nil)
     (define-key helm-projectile-projects-map (kbd "C-/") 'raz-helm-projectile-rg)
-    (helm-add-action-to-source "ripgrep" 'razzi-helm-run-project-search helm-source-projectile-projects)))
+    (define-key helm-projectile-projects-map (kbd "C-;") 'raz-helm-projectile-magit-status)
+    (helm-add-action-to-source "ripgrep" 'razzi-helm-run-project-search helm-source-projectile-projects)
+    (helm-add-action-to-source "magit" 'razzi-helm-run-magit-status helm-source-projectile-projects)))
