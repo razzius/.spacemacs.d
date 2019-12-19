d85ae10d744dde313433a4c58217cf8a34d7fb8c
Razzi Abuissa <razzi53@gmail.com>
2019-12-19 14:24:00 -0600

    Dumb jump no confirm

--- layers/razzi-dumb-jump/packages.el
+++ layers/razzi-dumb-jump/packages.el
@@ -3,4 +3,6 @@
 (defun razzi-dumb-jump/post-init-dumb-jump ()
   (use-package dumb-jump
     :init
-    (setq dumb-jump-force-searcher 'rg)))
+    (setq
+     dumb-jump-confirm-jump-to-modified-file nil
+     dumb-jump-force-searcher 'rg)))
